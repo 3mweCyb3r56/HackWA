33a144ed3b7f2d1b30fb459e78a8ff79a2884fdf
emwe56 <73535922+emwe56@users.noreply.github.com>
2020-10-28 11:00:22 +0700

    Add files via upload

---
+++ HackWA.sh
@@ -0,0 +1,31 @@
+clear
+red='\033[31;1m'
+green='\033[32;1m'
+yellow='\033[33;1m'
+blue='\033[34;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+white='\033[37;1m'
+sleep 1
+echo
+figlet "HACK WHATSAPP"
+echo $red"By: 3mweCyb3r56"
+echo
+sleep 2
+echo $yellow"Masukan No WA Korban: "
+read korban
+sleep 1
+echo $cyan"Mohon Tunggu..."
+echo
+sleep 15
+echo $purple"Sedang Meminta Kode..."
+echo
+sleep 2
+echo $green"Kode No WA: 528163"
+echo
+sleep 1
+echo $white"Terima Kasih Telah Menggunakan Tools Ini"
+echo
+sleep 2
+exit
+fi
